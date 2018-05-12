4b63156e6ab09d1d160483f898ce9efa7d7d03be
Anton.afonin <dlya.glupostey@gmail.com>
2018-05-12 18:27:11 +0300

    added comments

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
+# Импортируем нужные компоненты
 from telegram.ext import Updater
 
+# Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 def main():
-    mybot = Updater("587083051:AAEjr2LTJc224qZjRyuFTED059vnYHJ0Qt8")
-    mybot.start_polling()
-    mybot.idle()
+	mybot = Updater("587083051:AAEjr2LTJc224qZjRyuFTED059vnYHJ0Qt8")
+	mybot.start_polling()
+	mybot.idle()
+
+# Вызываем функцию - эта строчка собственно запускает бота
+main()
