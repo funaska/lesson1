066b7d2f1e1ebe1436fcbe14db3f418b2c38ec4d
Anton.afonin <dlya.glupostey@gmail.com>
2018-05-12 18:42:19 +0300

    added greeting

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 # Импортируем нужные компоненты
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
+
+# Настройки прокси
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+def greet_user(bot, update):
+	print('Привет!')
 
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 def main():
-	mybot = Updater("587083051:AAEjr2LTJc224qZjRyuFTED059vnYHJ0Qt8")
+	mybot = Updater("587083051:AAEjr2LTJc224qZjRyuFTED059vnYHJ0Qt8", request_kwargs=PROXY)
+
+	dp = mybot.dispatcher
+	dp.add_handler(CommandHandler("start", greet_user))
 	mybot.start_polling()
 	mybot.idle()
 
