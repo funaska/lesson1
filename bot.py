92d8167ccaa9da2529ab8b1ae4176df4ab0e77ac
Anton.afonin <dlya.glupostey@gmail.com>
2018-05-12 19:04:01 +0300

    добавлена обработка сообщений

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # Импортируем нужные компоненты
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Настройки прокси
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
@@ -16,12 +16,18 @@ def greet_user(bot, update):
 	update.message.reply_text(text)
 	# print('Привет!')
 
+def talk_to_me(bot, update):
+	user_text = update.message.text
+	print('Пользователь написал: ' + user_text)
+	update.message.reply_text('Ваше обращение будет обработано')
+
 # Функция, которая соединяется с платформой Telegram, "тело" нашего бота
 def main():
 	mybot = Updater("587083051:AAEjr2LTJc224qZjRyuFTED059vnYHJ0Qt8", request_kwargs=PROXY)
 
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 	mybot.start_polling()
 	mybot.idle()
 
